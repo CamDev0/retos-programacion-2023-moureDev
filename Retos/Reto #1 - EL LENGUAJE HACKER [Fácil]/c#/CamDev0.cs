2e57a9407a7c9fd93a859ee280266b9080bb4a11
Camilo Barrios <102267020+CamDev0@users.noreply.github.com>
2023-02-24 21:25:08 -0500

    Reto #1 mouredev - C#

---
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/c#/CamDev0.cs
@@ -0,0 +1,29 @@
+﻿
+class Reto1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("**LENGUAJE DEL HAKER**");
+
+        string txt = "";
+
+        Console.WriteLine("\nIngrese el texto que se quiere convertir: ");
+        txt = Console.ReadLine();
+
+        txt = txt.ToLower().Replace("a", "4").Replace("b", "I3").Replace("c", "[").Replace("d", ")").Replace("e", "3")
+                            .Replace("f", "|=").Replace("g", "&").Replace("h", "#").Replace("i", "1").Replace("j", ",_|")
+                            .Replace("k", ">|").Replace("l", "1").Replace("m", "/\\/\\")
+                            .Replace("n", "^/").Replace("o", "0").Replace("p", "|*").Replace("q", "(_,)")
+                            .Replace("r", "I2").Replace("s", "5").Replace("t", "7").Replace("u", "(_)").Replace("v", "\\/")
+                            .Replace("w", "\\/\\/").Replace("x", "><").Replace("y", "j").Replace("z", "2");
+                            
+                            //No me funciona bien con los números :/
+                            //.Replace("0", "o").Replace("1", "L").Replace("2", "R").Replace("3", "E").Replace("4", "A")
+                            //.Replace("5", "S").Replace("6", "b").Replace("7", "T").Replace("8", "B").Replace("9", "g");
+        
+        Console.WriteLine("\n***TEXTO CONVERTIDO***\n");
+        Console.WriteLine(txt);
+
+        
+    }
+}
