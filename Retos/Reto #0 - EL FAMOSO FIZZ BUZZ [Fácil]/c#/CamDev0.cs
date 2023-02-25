94988a9733eeca0fe112a6bf65d862b099a7bfba
Camilo Barrios <102267020+CamDev0@users.noreply.github.com>
2023-02-24 20:11:30 -0500

    reto #0

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c#/CamDev0.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reto0
+{
+    class CamDev0
+    {
+        static void Main(string[] args)
+        {
+            int aux = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                aux = i;
+                
+               if  (aux % 3 == 0 && aux % 5 == 0)
+               {
+                    Console.WriteLine("fizzbuzz");
+               }
+               else if (aux % 3 == 0)
+               {
+                    Console.WriteLine("fizz");
+               }
+               else if (aux % 5 == 0)
+
+               {
+                    Console.WriteLine("buzz");
+               }
+               else
+                {
+                    Console.WriteLine(aux);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
